e59a37b4c3dfa03d8a5f2181f2f6f2a2306337b5
Clinton Judy <clinton@judy.io>
2024-08-28 13:24:40 -0400

    Remove all references to example plugin

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -8,7 +8,6 @@ void init(Plugin* p) {
 	pluginInstance = p;
 
 	// Add modules here
-	// p->addModel(modelMyModule);
 	p->addModel(modelRangeExtender);
 
 	// Any other plugin initialization may go here.
